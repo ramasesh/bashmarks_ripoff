a596f015906c8fc6fda5851855bc0d4f9792f3e1
Vinay Ramasesh <ramasesh@berkeley.edu>
2020-01-05 18:01:48 -0600

    sent all messages explicitly to /dev/tty, should allow to run this noninteractively

--- bashmarks.sh
+++ bashmarks.sh
@@ -20,31 +20,31 @@ function try_name {
 
 function bashmark {
   if [ -z $1 ]; then
-    echo "A name must be provided for the bashmark"
+    echo "A name must be provided for the bashmark" > /dev/tty
   else
-    echo "Bashmark will be set"
-    echo "Bashmark: $1 will now point to $PWD"
+    echo "Bashmark will be set" > /dev/tty
+    echo "Bashmark: $1 will now point to $PWD" > /dev/tty
     echo "$1:$PWD" >> $BASHMARK_FILE
   fi
 }
 
 function gotobashmark {
   if [ -z $1 ]; then 
-    echo "A bashmark name must be provided to go to"
+    echo "A bashmark name must be provided to go to" > /dev/tty
   else
     check_if_bashmark $1
     if [ -z "$pointed_directory" ]; then 
-      echo "Sorry, bashmark was not valid"
+      echo "Sorry, bashmark was not valid" > /dev/tty
     else
       if [ -d "$pointed_directory" ]; then 
         cd $pointed_directory
         if [ ! $? -eq 0 ]; then
-          echo "Changing into the bashmark directory seems to have failed."
-          echo "   You might want to check which directory you are in."
-          echo "   This is likely a permissions issue." 
+          echo "Changing into the bashmark directory seems to have failed." > /dev/tty
+          echo "   You might want to check which directory you are in." > /dev/tty
+          echo "   This is likely a permissions issue." > /dev/tty
         fi 
       else
-        echo "The directory the bashmark referred to, $pointed_directory, must no longer exist"
+        echo "The directory the bashmark referred to, $pointed_directory, must no longer exist" > /dev/tty
       fi
     fi
   fi
@@ -56,14 +56,14 @@ function list_bashmarks {
 
 function check_if_bashmark {
   if [ -z $1 ]; then 
-    echo "A man needs a name" 
+    echo "A man needs a name" > /dev/tty
   else
     if [ `cat $BASHMARK_FILE | grep ^$1: | wc -l` == 1 ]; then
-      echo "We have a valid bashmark!"
+      echo "We have a valid bashmark!" > /dev/tty
       cat $BASHMARK_FILE | grep ^$1: | cut -d':' -f 2
       pointed_directory=`cat $BASHMARK_FILE | grep ^$1: | cut -d':' -f 2`
     else
-      echo "We don't have a valid bashmark!"
+      echo "We don't have a valid bashmark!" > /dev/tty
       unset pointed_diretory
     fi
   fi
@@ -71,13 +71,13 @@ function check_if_bashmark {
 
 function delete_bashmark {
   if [ -z $1 ]; then 
-    echo "You didn't give me a bashmark to delete!"
+    echo "You didn't give me a bashmark to delete!" > /dev/tty
   else 
     check_if_bashmark $1
     if [ -z $pointed_directory ]; then
-      echo "The bashmark you wanted me to delete is not valid"
+      echo "The bashmark you wanted me to delete is not valid" > /dev/tty
     else 
-      echo "Deleting bashmark"
+      echo "Deleting bashmark" > /dev/tty
       cat $BASHMARK_FILE | sed "/^$1:/d" > tempfile
       mv tempfile $BASHMARK_FILE
     fi
@@ -87,7 +87,7 @@ function delete_bashmark {
 function bashmark_shortcut {
   if [ $# -eq 0 ]; then
     # without arguments, lists all the current bashmark shortcuts
-    echo "Current bashmark shortcuts:"
+    echo "Current bashmark shortcuts:" > /dev/tty
     cat $BASHMARK_FILE | grep COMMAND_SHORTCUT | cut -d" " -f2,3 | tr " " ":"
   else
     if [ $# -eq 2 ]; then
@@ -96,11 +96,11 @@ function bashmark_shortcut {
       if [ "gotobashmark" == $1 ] || [ "list_bashmarks" == $1 ] || [ "delete_bashmark" == $1 ] || [ "bashmark" == $1 ]; then
         # TODO For the above condition, there should be a way to write it more cleanly
         # with a grep, but I couldn't get it to work earlier.
-        echo "Setting bashmark shortcut for command $1"
+        echo "Setting bashmark shortcut for command $1" > /dev/tty
 
         # check if there was an old alias, and if so, unalias it
         old_alias=`grep "COMMAND_SHORTCUT $1" $BASHMARK_FILE | cut -d" " -f3`
-        echo "Old alias, which will now be unaliased, was $old_alias"
+        echo "Old alias, which will now be unaliased, was $old_alias" > /dev/tty
         unalias $old_alias 2> /dev/null
 
         cat $BASHMARK_FILE | grep -v "COMMAND_SHORTCUT $1" > $BASHMARK_FILE.tmp
@@ -109,9 +109,9 @@ function bashmark_shortcut {
 
         # Now set new alias
         alias $2=$1
-        echo "New alias is $2"
+        echo "New alias is $2" > /dev/tty
       else
-        echo "First argument must be one of [gotobashmark, list_bashmarks, delete_bashmark]"
+        echo "First argument must be one of [gotobashmark, list_bashmarks, delete_bashmark]" > /dev/tty
         exit 1
       fi 
     fi 
